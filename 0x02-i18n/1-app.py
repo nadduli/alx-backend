c075084df9d1bd574e6de5c4993831409a4de697
nadduli <naddulidaniel94@gmail.com>
2022-05-11 13:22:19 +0300

    added task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 """A Basic Flask app.
 """
-
-from flask import Flask, render_template
 from flask_babel import Babel
+from flask import Flask, render_template
+
 
 class Config:
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
+babel = Babel(app)
+
 
 @app.route('/')
 def get_index() -> str:
-    """The index page.
+    """The home/index page.
     """
     return render_template('1-index.html')
 
